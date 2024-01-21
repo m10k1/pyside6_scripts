e6d6e7bd8fcbd7bcacffc988563836708c85f064
Motoki Matsumoto <mtmotoki@gmail.com>
2024-01-21 11:28:43 +0900

    add mdi feature

--- MDIApp.py
+++ MDIApp.py
@@ -1,12 +1,43 @@
 import sys
 from PySide6.QtWidgets import (
-    QApplication,QMainWindow
+    QApplication,QMainWindow, 
+    QMdiArea, QMdiSubWindow, QTextEdit
 )
+from PySide6.QtGui import QAction
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("My App")
+        self.setGeometry(100, 100, 800, 600)
+        self.create_menu()
+
+        self.mid_area = QMdiArea()
+        self.setCentralWidget(self.mid_area)
+
+
+    def create_menu(self):
+        menu_bar = self.menuBar()
+
+        # file menu
+        file_menu = menu_bar.addMenu("ファイル")
+
+        open_action = QAction("開く", self)
+        open_action.triggered.connect(self.open_file)
+        file_menu.addAction(open_action)
+    
+    def create_sub_window(self):
+        sub_window = QMdiSubWindow()
+        text_edit = QTextEdit()
+        sub_window.setWidget(text_edit)
+        sub_window.setWindowTitle("New Document")
+        self.mid_area.addSubWindow(sub_window)
+        sub_window.show()
+
+
+    def open_file(self):
+        self.create_sub_window()
+
 
 def main():
     app = QApplication(sys.argv)
